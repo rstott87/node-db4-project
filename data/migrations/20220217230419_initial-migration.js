b78b7b08dc6525654dcaec3485a5a84d19db635a
rstott87 <rstott87@yahoo.com>
2022-02-17 18:44:57 -0500

    created steps

--- data/migrations/20220217230419_initial-migration.js
+++ data/migrations/20220217230419_initial-migration.js
@@ -11,7 +11,17 @@ exports.up = async function (knex) {
         table.string('ingredient_unit', 50)
     })
     .createTable('steps', table => {
-        table.increments()
+        table.increments('ingredient_id')
+        table.string('ingredient_name', 200).notNullable().unique()
+        table.interger('step_number').notNullable()
+        table.interger('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('recipe_id')
+            .inTable('recipes')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        
     })
     .createTable('step_ingredients', table => {
         table.increments()
