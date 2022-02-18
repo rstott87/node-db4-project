adea95059f495fb19af7a7af8b3cdc9260a1a4a0
rstott87 <rstott87@yahoo.com>
2022-02-17 19:21:45 -0500

    added forgeign keys

--- data/migrations/20220217230419_initial-migration.js
+++ data/migrations/20220217230419_initial-migration.js
@@ -13,8 +13,8 @@ exports.up = async function (knex) {
     .createTable('steps', table => {
         table.increments('ingredient_id')
         table.string('ingredient_name', 200).notNullable().unique()
-        table.interger('step_number').notNullable()
-        table.interger('recipe_id')
+        table.integer('step_number').notNullable()
+        table.integer('recipe_id') 
             .unsigned()
             .notNullable()
             .references('recipe_id')
@@ -24,7 +24,22 @@ exports.up = async function (knex) {
         
     })
     .createTable('step_ingredients', table => {
-        table.increments()
+        table.increments('step_ingredients_id')
+        table.float('quantity').notNullable()
+        table.integer('step_id')   //note foreign key
+            .unsigned()
+            .notNullable()
+            .references('step_id')   // shows that it refs step id in steps
+            .inTable('steps')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        table.integer('ingredient_id')
+            .unsigned()
+            .notNullable()
+            .references('ingredient_id')  // shows taht it refs ingredient id in ingredients
+            .inTable('ingredients')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
     })
 };
 
